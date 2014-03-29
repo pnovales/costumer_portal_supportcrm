98c5ea1b661c887317057fe8ac7026815020d0f2
Peter <peter@peter-Inspiron-N5050.(none)>
2014-03-28 22:34:17 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CostumerPortalSupportcrm::Application.config.secret_key_base = '10e5e1e065a844b0244d9a260a4c28509cc9abe9b08e8e6239e20daa1a5b1d1cf20ea99db8de676b946d506f78f90239e0bc4dc84cc8e69b71c91f0cfb7e7a04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CostumerPortalSupportcrm::Application.config.session_store :cookie_store, key: '_costumer_portal_supportcrm_session'
